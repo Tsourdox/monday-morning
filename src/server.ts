489aa3225d0d67c790b5a752c49801c451ff41c3
David Jensen <tsourdox@icloud.com>
2022-05-30 10:59:46 +0200

    set node engine and update server file

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const secret = process.env.SECRET || "";
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello world " + secret);
 });
 
 app.listen(port, () => {
