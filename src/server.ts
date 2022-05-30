52fe6214952a59c6459b9507e050b4c50b077ff8
David Jensen <tsourdox@icloud.com>
2022-05-30 11:36:38 +0200

    updates...

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const secret = process.env.SECRET || "";
+const secret = process.env.SECRET || "No Secret";
 
 app.get("/", (req, res) => {
-  res.send("hello world " + secret);
+  res.send("hello world, this is a secret for you: " + secret);
 });
 
 app.listen(port, () => {
