3c407ea908ee6db8cc8ce8d02f1ff1464d2901b1
David Jensen <tsourdox@icloud.com>
2022-05-30 09:47:41 +0200

    use env port variable

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(3000, () => {
-  console.log("Server is upp an running on: http://localhost:3000");
+app.listen(port, () => {
+  console.log(`Server is upp an running on: http://localhost:${port}`);
 });
